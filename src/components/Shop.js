5a0735e2ca9d13c140460495f29d678e8b482ef5
Ramon Nunes Morais <20211074010027@ifrn.local>
2024-12-05 07:57:40 -0300

    ATUALIZADO 2024.V5!!!!

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,8 @@
-import { useContext, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import Product from "./Product";
 import { CircularProgress } from "@mui/material";
 import { CartContext } from "../context/CartContext";
 import styles from "./Shop.module.css";
-import { LocationSearchingOutlined } from "@mui/icons-material";
 import { useState } from "react";
 
 export default function Shop() {
@@ -13,45 +12,55 @@ export default function Shop() {
     const searchInput = useRef("");
     const [filteredItems, setFilteredItems] = useState([]);
 
-    function handleSearch () {
+    useEffect(() => {
+        if (products) {
+            setFilteredItems(products);
+        }
+    }, [products]);
+
+    function handleSearch() {
         const term = searchInput.current.value.toLowerCase();
         setFilteredItems(
             products.filter((item) => item.title.toLowerCase().includes(term))
         );
     }
-    function clearSearch () {
+    function clearSearch() {
         searchInput.current.value = "";
+        setFilteredItems(products);
     }
 
     return (
         <section id="shop">
             <h2>Elegant Products for Everyone</h2>
 
-<div className={styles.search_container}>
-<div className={styles.search_box}> 
-    <input 
-    ref={searchInput}
-    className={styles.search_input}
-    type="text"
-    placeholder="Type to search..."
-    onChange={handleSearch}/>
-    <button className={styles.search_clear} onClick={clearSearch}>CLEAR</button>
-</div>
-</div>
+            <div className={styles.search_container}>
+                <div className={styles.search_box}>
+                    <input
+                        ref={searchInput}
+                        className={styles.search_input}
+                        type="text"
+                        placeholder="Type to search..."
+                        onChange={handleSearch} />
+                    <button className={styles.search_clear} onClick={clearSearch}>CLEAR</button>
+                </div>
+            </div>
 
             <ul id="products">
                 {error && <p>{error}</p>}
-                {!loading && products ? (
-                    products.map((product) => (
+                {loading &&
+                    <div id="loading">
+                        <CircularProgress size="10rem" color="inherit" />
+                        <p>Loading products...</p>
+                    </div>
+                }
+                {!loading && !error && filteredItems.length > 0 ? (
+                    filteredItems.map((product) => (
                         <li key={product.id}>
                             <Product {...product} />
                         </li>
                     ))
                 ) : (
-                    <div id="loading">
-                        <CircularProgress size="10rem" color="inherit" />
-                        <p>Loading products...</p>
-                    </div>
+                    <p>not found</p>
                 )}
             </ul>
 
